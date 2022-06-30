5885a04c75321825e77f5339d92924a25e2918b5
Danieel Carvalho <Danielscg2012@gmail.com>
2022-06-30 15:32:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let movie1 = [ "Comedy", {title: "Die Hard", director: "John McTiernan", year: 1988 }]
+let movie2 = ["Action", {title: "Blade Runner(THE DIRECTOR'S VERSION)",director:" Ridley Scott", year: 1982 }]
+let movie3 = ["Fantasy" ,{ title: "Lord of the Rings: The Fellowship of the Ring", director: "Peter Jackson", year: 2001}]
+
+
+
+if(movie2[1].title === "Blade Runner(THE DIRECTOR'S VERSION)" ){
+    console.log("MY FAVORITE MOVIE IS THIS")
+} else{
+    console.log("NOT EVEN 5 STARS...")
+}
